e4b5359e93fa1852c903615081b0fbb5d450f124
mario-sica/Mario <mario97sica@gmail.com>
2024-06-21 11:10:10 +0200

    Fix: added missing exercise

--- DOM Manipulation/Manipolazione del DOM-1/index.js
+++ DOM Manipulation/Manipolazione del DOM-1/index.js
@@ -1,3 +1,15 @@
 const addProduct = () => {
-  // ...
+  const input = document.getElementsByTagName("input")[0];
+  const inputValue = input.value;
+
+  const li = document.createElement("li");
+  li.innerText = inputValue;
+
+  const input1 = document.createElement("input");
+  input1.type = "checkbox";
+
+  li.appendChild(input1);
+
+  const ul = document.getElementsByTagName("ul")[0];
+  ul.appendChild(li);
 };
